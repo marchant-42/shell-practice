464f741d618f6364197c6716ea64b5ea64eabd30
satish <ravipallisatish42@gmail.com>
2025-06-04 20:21:25 +0530

    hello world started

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,5 @@
-PERSON1=trump
-PERSON2=musk
+PERSON1=modi
+PERSON2=satish
 echo "$PERSON1: hey $PERSON2 what are you doing"
 echo "$PERSON2: I'm working on my new rocket"
 echo "$PERSON1: oh really? can I help?"
