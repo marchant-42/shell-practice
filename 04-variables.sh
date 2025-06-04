8a507d3857a41363cdd32700e1caedc7f42b002d
satish <ravipallisatish42@gmail.com>
2025-06-04 20:24:37 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#sending arguments outside
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: hey $PERSON2 what are you doing"
+echo "$PERSON2: I'm working on my new rocket"
+echo "$PERSON1: oh really? can I help?"
+echo "$PERSON2: sure, I could use some help with the launch"
+echo "$PERSON1: great! when is the launch?"
+echo "$PERSON2: it's scheduled for next month"
