89bdca7c52eab81ac0aea3061b15a9b11b7df3a7
satish <ravipallisatish42@gmail.com>
2025-06-04 20:19:31 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+PERSON1=trump
+PERSON2=musk
+echo "$PERSON1: hey $PERSON2 what are you doing"
+echo "$PERSON2: I'm working on my new rocket"
+echo "$PERSON1: oh really? can I help?"
+echo "$PERSON2: sure, I could use some help with the launch"
+echo "$PERSON1: great! when is the launch?"
+echo "$PERSON2: it's scheduled for next month"
