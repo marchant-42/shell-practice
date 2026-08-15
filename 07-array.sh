51a988ce40c016fa3b2a48b732bc5db63176185f
satish <ravipallisatish42@gmail.com>
2026-08-15 19:41:02 +0530

    what are you doing musk

--- 07-array.sh
+++ 07-array.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "trump: hey musk what are you doing"
-echo "musk: I'm working on my new rocket"
-echo "trump: oh really? can I help?"
-echo "musk: sure, I could use some help with the launch"
-echo "trump: great! when is the launch?"
-echo "musk: it's scheduled for next month"
+MOVIES=("court","hit3", "pushpa2","thandel")
+echo "First movie: ${MOVIES[0]}"
