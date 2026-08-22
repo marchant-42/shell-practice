bc94a44519c14f0dc6fd1264bc5f61539c1913fe
satish <ravipallisatish42@gmail.com>
2026-08-22 17:57:31 +0530

    hello-world

--- 14-logs.sh
+++ 14-logs.sh
@@ -8,40 +8,40 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-echo "satish"
+
 mkdir -p $LOGS_FOLDER
-echo "script statrted excuted at : $(date)" &>>$LOGS_FILE
+echo "script statrted excuted at : $(date)" | tee -a $LOGS_FILE
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Error : only root user can run this script $N" &>>$LOGS_FILE
+    echo -e "$R Error : only root user can run this script $N" | tee -a $LOGS_FILE
     exit 1
 else 
-    echo "you are running with root access" &>>$LOGS_FILE
+    echo "you are running with root access" | tee -a $LOGS_FILE
 fi
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "installing $2 is ...$G success $N" &>>$LOGS_FILE
+        echo -e "installing $2 is ...$G success $N" | tee -a $LOGS_FILE
     else
-        echo -e "installing $2 is $R failed $N" &>>$LOGS_FILE
+        echo -e "installing $2 is $R failed $N" | tee -a $LOGS_FILE
         exit 1
     fi
 }
 dnf list installed mysql
 if [ $? -ne 0 ] 
 then
-    echo "MySQL is not installed, proceeding with installation" &>>$LOGS_FILE
-    dnf install mysql -y &>>$LOGS_FILE
+    echo "MySQL is not installed, proceeding with installation" | tee -a $LOGS_FILE
+    dnf install mysql -y | tee -a $LOGS_FILE
     VALIDATE $? "MySQL"
 else
-    echo -e "MySQL is $Y already installed nothing to do $N" &>>$LOGS_FILE
+    echo -e "MySQL is $Y already installed nothing to do $N" | tee -a $LOGS_FILE
 fi
 dnf list installed python3
 if [ $? -ne 0 ] 
 then
-    echo "python3 is not installed, proceeding with installation" &>>$LOGS_FILE
-    dnf install python3 -y &>>$LOGS_FILE
+    echo "python3 is not installed, proceeding with installation" | tee -a $LOGS_FILE
+    dnf install python3 -y | tee -a $LOGS_FILE
     VALIDATE $? "python3"
 else
-    echo -e "nothing to do python $Y already installed $N" &>>$LOGS_FILE
+    echo -e "nothing to do python $Y already installed $N" | tee -a $LOGS_FILE
 fi
