7522447f8b286c85849c6cf5fa82c48fd8b2eeb8
satish <ravipallisatish42@gmail.com>
2026-08-22 17:17:55 +0530

    hello-world

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,48 +5,43 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "satisk"
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGS_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script statrted excuted at : $(date)" &>>$LOGS_FILE
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Error : only root user can run this script $N"
+    echo -e "$R Error : only root user can run this script $N" &>>$LOGS_FILE
     exit 1
 else 
-    echo "you are running with root access"
+    echo "you are running with root access" &>>$LOGS_FILE
 fi
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e "installing $2 is ...$G success $N"
+        echo -e "installing $2 is ...$G success $N" &>>$LOGS_FILE
     else
-        echo -e "installing $2 is $R failed $N"
+        echo -e "installing $2 is $R failed $N" &>>$LOGS_FILE
         exit 1
     fi
 }
 dnf list installed mysql
 if [ $? -ne 0 ] 
 then
-    echo "MySQL is not installed, proceeding with installation"
-    dnf install mysql -y
+    echo "MySQL is not installed, proceeding with installation" &>>$LOGS_FILE
+    dnf install mysql -y &>>$LOGS_FILE
     VALIDATE $? "MySQL"
 else
-    echo -e "MySQL is $Y already installed nothing to do $N"
+    echo -e "MySQL is $Y already installed nothing to do $N" &>>$LOGS_FILE
 fi
 dnf list installed python3
 if [ $? -ne 0 ] 
 then
-    echo "python3 is not installed, proceeding with installation"
-    dnf install python3 -y
+    echo "python3 is not installed, proceeding with installation" &>>$LOGS_FILE
+    dnf install python3 -y &>>$LOGS_FILE
     VALIDATE $? "python3"
     
 else
-    echo "python3 is already installed nothing to do"
-fi
-dnf list installed Ngnix
-if [ $? -ne 0 ] 
-then
-    echo "Nginx is not installed, proceeding with installation"
-    dnf install Nginx -y
-    VALIDATE $? "Nginx"
-else
-    echo "Nginx is already installed nothing to do"
-fi
+    echo "python3 is already installed nothing to do" &>>$LOGS_FILE
